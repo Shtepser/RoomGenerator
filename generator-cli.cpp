0ecec770cb3376fcd84088d4d012e0bb1e0a19f3
Anatoliy Poletaev <shtepser@yandex.ru>
2022-01-19 14:53:43 +0300

    Small CLI refactoring

--- generator-cli.cpp
+++ generator-cli.cpp
@@ -5,23 +5,47 @@
 #include "RoomContent.hpp"
 
 
+enum ParseStatus {SUCCESS, INVALID_ARGUMENTS, INVALID_N};
+
+
+void parse_arguments(int argc, char** argv,
+                     unsigned int *n, ParseStatus *status)
+{
+    char *endptr;
+    long n_argument;
+    
+    if (argc != 2)
+    {
+        *status = INVALID_ARGUMENTS;
+        return;
+    }
+    n_argument = strtol(argv[1], &endptr, 10);
+    if (endptr == argv[1] || n_argument < 0 || *endptr != '\0')
+    {
+        *status = INVALID_N;
+        return;
+    }
+    *n = n_argument;
+    *status = SUCCESS;
+}
+
 int main(int argc, char** argv)
 {
-    if (argc != 2)
+    ParseStatus arguments_status = SUCCESS;
+    unsigned int times = 0;
+
+    parse_arguments(argc, argv, &times, &arguments_status);
+
+    if (arguments_status == INVALID_ARGUMENTS)
     {
         printf("Usage: generator-cli N\n* N: number of rooms to generate content for\n");
         return 0;
     }
-
-    char *endptr;
-    int times_argument = strtol(argv[1], &endptr, 10);
-    if (endptr == argv[1] || times_argument < 0 || *endptr != '\0')
+    if (arguments_status == INVALID_N)
     {
         printf("Invalid argument!\n");
         return 1;
     }
-
-    unsigned int times = unsigned(times_argument);
     
     init_random();
     
